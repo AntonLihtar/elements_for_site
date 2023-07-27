a4c9e55c8845acbbb1ec95694cd1b7749c779b9f
Antonl1 <sa-fa@mail.ru>
2023-07-27 21:05:13 +0300

    add function

--- gallery/gallery1/app.js
+++ gallery/gallery1/app.js
@@ -1,11 +1,16 @@
-const slides = document.querySelectorAll('.slide')
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+function pluginSlides(indexSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+    slides[indexSlide].classList.add('active')
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((el) => el.classList.remove('active'))
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((el) => el.classList.remove('active'))
-}
+pluginSlides(2)
