1b721e906c528ff162f86224411d41b35eadeee0
Antonl1 <sa-fa@mail.ru>
2023-07-30 19:45:52 +0300

    add mani app

--- sliders/simple_slider_through_innerHtml/js/script.js
+++ sliders/simple_slider_through_innerHtml/js/script.js
@@ -2,11 +2,11 @@ const prevBtn = document.querySelector('.slider__prev');
 const nextBtn = document.querySelector('.slider__next');
 const outImg = document.querySelector('.slider__img');
 
-const art1 = '../img/1.jpg';
-const art2 = '../img/2.jpg';
-const art3 = '../img/3.jpg';
-const art4 = '../img/2.jpg';
-const art5 = '../img/3.jpg';
+const art1 = 'img/1.jpg';
+const art2 = 'img/2.jpg';
+const art3 = 'img/3.jpg';
+const art4 = 'img/2.jpg';
+const art5 = 'img/3.jpg';
 
 //создаем массив изображений
 const images = [art1, art2, art3, art4, art5];
